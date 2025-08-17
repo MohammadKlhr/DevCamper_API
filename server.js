057738cf8d393f2a7c6058f50cf04bef378fd6f0
MohammadKlhr <mohammad.mahdi.kalhor.99@gmail.com>
2025-08-17 13:07:37 +0330

    Creating Router for Bootcamps Route

--- server.js
+++ server.js
@@ -1,44 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route Files
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  // res.send('<h1>Hello from express!</h1>');
-  // res.send({ first_name: 'Brad', last_name: 'Traversy' });
-  // res.json({ first_name: 'Brad', last_name: 'Traversy' });
-  // res.sendStatus(404)
-  res.status(200).json({ success: true, data: { id: 1, name: 'Brad' } });
-});
-
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all Bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show Bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create a new Bootcamps' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
-});
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
