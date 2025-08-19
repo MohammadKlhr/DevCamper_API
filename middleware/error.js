e2a95b41e8c065a764ace3ddaaa0431cdd56feff
MohammadKlhr <mohammad.mahdi.kalhor.99@gmail.com>
2025-08-20 02:48:51 +0330

    Mongoose Validation and Duplication ErrorHandler

--- middleware/error.js
+++ middleware/error.js
@@ -1,16 +1,28 @@
-const ErrorResponse = require("../utils/errorResponse");
+const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
-  let error = { ...err };  //takes all enumerable own properties from err and copies them into a new plain object
-  error.message = err.message;  //message, stack and name are not copied because they are not enumerable 
+  let error = { ...err }; //takes all enumerable own properties from err and copies them into a new plain object
+  error.message = err.message; //message, stack and name are not copied because they are not enumerable
 
   //Log to console for the developer
-  console.log(err.stack.red);
+  console.log(err);
 
   // Mongoose Bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
-    error = new ErrorResponse(message, 404)
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate Key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation Error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
