694c54b18cecaaa4e3ea02cbceae445b2e983a6d
MohammadKlhr <mohammad.mahdi.kalhor.99@gmail.com>
2025-08-23 02:22:04 +0330

    Adding Slug by Mongoose Middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,40 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please add a name"],
+    required: [true, 'Please add a name'],
     unique: true,
     trim: true,
-    maxLength: [50, "Name can not be more than 50 characters"],
+    maxLength: [50, 'Name can not be more than 50 characters'],
   },
   slug: String,
   description: {
     type: String,
-    required: [true, "Please add a description"],
-    maxLength: [500, "Description can not be more than 500 characters"],
+    required: [true, 'Please add a description'],
+    maxLength: [500, 'Description can not be more than 500 characters'],
   },
   website: {
     type: String,
     match: [
       /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-      "Please use a valid URL with HTTP or HTTPS",
+      'Please use a valid URL with HTTP or HTTPS',
     ],
   },
   phone: {
     type: String,
-    maxLength: [20, "Phone number can not be longer than 20 characters"],
+    maxLength: [20, 'Phone number can not be longer than 20 characters'],
   },
   email: {
     type: String,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please Add a valid email",
+      'Please Add a valid email',
     ],
   },
   address: {
     type: String,
-    required: [true, "Please add an address"],
+    required: [true, 'Please add an address'],
   },
   /* location: {
     //GeoJSON Point
@@ -60,44 +61,50 @@ const BootcampSchema = new mongoose.Schema({
     type: [String],
     required: true,
     enum: [
-      "Web Development",
-      "Mobile Development",
-      "UI/UX",
-      "Data Science",
-      "Business",
-      "Other",
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other',
     ],
   },
   averageRating: {
     type: Number,
-    min: [1, "Rating must be at least 1"],
-    max: [10, "Rating cannot be more than 10"],
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating cannot be more than 10'],
   },
   averageCost: Number,
   photo: {
     type: String,
-    default: 'no-photo.jpg'
+    default: 'no-photo.jpg',
   },
   housing: {
     type: Boolean,
-    default: false
+    default: false,
   },
   jobAssistance: {
     type: Boolean,
-    default: false
+    default: false,
   },
   jobGuarantee: {
     type: Boolean,
-    default: false
+    default: false,
   },
   acceptGi: {
     type: Boolean,
-    default: false
+    default: false,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Bootcamp', BootcampSchema)
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
