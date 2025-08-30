bef4a10ef066422e3195661f078bc7fcacb1ad37
MohammadKlhr <mohammad.mahdi.kalhor.99@gmail.com>
2025-08-31 00:36:00 +0330

    Adding ForgotPassword and ResetToken Functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,7 +53,40 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
-// Get token from model, create cookie and send response
+// @desc            Get current logged in user
+// @route           GET /api/v1/auth/me
+// @access          Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc            Forgot password
+// @route           POST /api/v1/auth/forgotpassword
+// @access          public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// Get token from model, create cookie and send response - Helper function
 const sendTokenResponse = (user, statusCode, res) => {
   //Create token
   const token = user.getSignedJwtToken();
@@ -74,15 +107,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie('token', token, options)
     .json({ success: true, token });
 };
-
-// @desc            Get current logged in user
-// @route           POST /api/v1/auth/me
-// @access          Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
